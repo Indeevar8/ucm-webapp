e0af933257f468f122dd4072c53977064f5b1efd
indeevar8 <ineevar.reddy9@gmail.com>
2019-01-28 21:55:13 -0600

    initial code base

---
+++ ucm/src/main/java/edu/ucm/UcmApplication.java
@@ -0,0 +1,14 @@
+package edu.ucm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcmApplication.class, args);
+	}
+
+}
+
